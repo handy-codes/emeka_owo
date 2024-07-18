274a04e502634a5ad6f2df373817dadb1e1cac01
Emeka <paxymek@gmail.com>
2024-07-18 14:41:40 +0100

    Update ImageUrls

--- src/data.js
+++ src/data.js
@@ -1,28 +1,28 @@
 export const products = [
     {
         id: 1,
-        img: "https://i.ibb.co/VjQsTzC/esaver3.png",
-        link: "https://esaver.netlify.app",  
+        img: "https://i.ibb.co/TbDg3c5/mekxus-Gofull.png",
+        link: "https://meksus-properties.onrender.com",  
     },
     {
         id: 2,
-        img: "https://i.ibb.co/pZjBtvV/owosocial.png",
-        link: "https://owo-social-app.netlify.app",
+        img: "https://i.ibb.co/6vRHg0x/x-Gofull.png",
+        link: "https://x-app-dusky.vercel.app",
     },
     {
         id: 3,
+        img: "https://i.ibb.co/JKt3pBf/owosocial-Gofull.png",
+        link: "https://owo-social-app.netlify.app",
+    },
+    {
+        id: 4,
         img: "https://i.ibb.co/YpqSnrg/owoportfolio.png",
         link: "https://emekaowo-portfolio.netlify.app",
     },
     {
-        id: 4,
-        img: "https://i.ibb.co/F60T6M2/owosocialb.png",
-        link: "https://owo-social-app.netlify.app",
-    },
-    {
         id: 5,
-        img: "https://i.ibb.co/znxkBwk/portfolio.png",
-        link: "https://handy-codes.github.io/owo-portfolio/",
+        img: "https://i.ibb.co/kMGjXGR/stopwatch-Gofull.png",
+        link: "https://handy-codes.github.io/digital-clock/",
     },
     {
         id: 6,
