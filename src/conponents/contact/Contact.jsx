c76c281885abad4fe08d7c1feb23a815da8af5f8
emeka <emekaowo@yahoo.com>
2024-08-06 13:04:49 +0100

    more fixes

--- src/conponents/contact/Contact.jsx
+++ src/conponents/contact/Contact.jsx
@@ -44,26 +44,26 @@ const Contact = () => {
             <div className="c-left">
                 <h1 className="c-title">Let's discuss your project</h1>
                 <div className="c-info text-xl font-bold mr-4">
-                    <div className="c-info-item text-[17px] sm:text-[20px] font-bold hover:text-[#964e0f] hover:translate-x-1 duration-300">
+                    <div className="c-info-item text-[18px] sm:text-[20px] font-bold hover:text-[#964e0f] hover:translate-x-1 duration-300">
                         <img src={phone} alt="phone_logo" className="c-icon rounded-full" />
                         +2349038984567
                     </div>
-                    <div className="c-info-item text-[17px] font-bold sm:text-[20px] hover:text-[#964e0f] hover:translate-x-1 duration-300">
+                    <div className="c-info-item text-[18px] font-bold sm:text-[20px] hover:text-[#964e0f] hover:translate-x-1 duration-300">
                         <img src={email} alt="email_logo" className="c-icon rounded-full border border-blue" />
                         paxymekventures@gmail.com
                     </div>
                     <div className="c-info-item">
                         <img src={linkedin} alt="linkedin_logo" className="c-icon rounded-full border border-blue-700" />
-                        <a className='text-[17px] font-bold underline sm:text-[20px] text-blue-600 hover:text-[#964e0f] hover:translate-x-1 duration-300' href="https://www.linkedin.com/in/emeka-owo-204aaa2a5">www.linkedin.com/in/emeka-owo-204aaa2a5</a> 
+                        <a className='text-[18px] font-bold underline sm:text-[20px] text-blue-600 hover:text-[#964e0f] hover:translate-x-1 duration-300' href="https://www.linkedin.com/in/emeka-owo-204aaa2a5">www.linkedin.com/in/emeka-owo-204aaa2a5</a> 
                     </div>    
                     <div className="c-info-item">
                         <img src={github2} alt="github_logo" className="c-icon rounded-full" />
-                        <a className='text-[17px] font-bold underline sm:text-[20px] text-blue-600 hover:text-[#964e0f] hover:translate-x-1 duration-300' href="https://github.com/handy-codes">www.github.com/handy-codes</a>
+                        <a className='text-[18px] font-bold underline sm:text-[20px] text-blue-600 hover:text-[#964e0f] hover:translate-x-1 duration-300' href="https://github.com/handy-codes">www.github.com/handy-codes</a>
                     </div>                  
                 </div>
             </div>
             <div className="c-right p-4">
-                <p className="c-desc">
+                <p className="c-desc inline text-[18px] font-bold sm:text-[20px]">
                     What's your project? Get in touch. Always
                     freelancing if the right project comes along.
                 </p>
