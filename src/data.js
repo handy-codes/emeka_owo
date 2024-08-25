c81ca2bcfe4a5e9ae685c3646c68f0abd681b914
emeka <emekaowo@yahoo.com>
2024-08-25 19:16:23 +0100

    adding zoom clone app

--- src/data.js
+++ src/data.js
@@ -6,41 +6,46 @@ export const products = [
     },
     {    
         id: 2,
+        img: "https://i.ibb.co/fvPxxDX/screencapture-owo-xoom-app-vercel-app-2024-08-25-19-06-07.png",
+        link: "https://owo-xoom-app.vercel.app/",
+    },
+    {    
+        id: 3,
         img: "https://i.ibb.co/GMpQqNt/screencapture-handy-codes-github-io-ui-design-test-portfolio-2024-08-03-19-55-44.png",
         link: "https://handy-codes.github.io/ui-design-test-portfolio/",
     },
     {
-        id: 3,
+        id: 4,
         img: "https://i.ibb.co/6vRHg0x/x-Gofull.png",
         link: "https://x-app-dusky.vercel.app",
     },
     {
-        id: 4,
+        id: 5,
         img: "https://i.ibb.co/QDnzPxY/screencapture-meksmart-netlify-app-2024-08-03-15-18-59.png",
         link: "https://meksmart.netlify.app/",
     },
     {
-        id: 5,
+        id: 6,
         img: "https://i.ibb.co/vvF6LCs/screencapture-handy-codes-github-io-digital-clock-2024-08-03-15-12-55.png",
         link: "https://handy-codes.github.io/digital-clock/",
     },
     {
-        id: 6,
+        id: 7,
         img: "https://i.ibb.co/YpqSnrg/owoportfolio.png",
         link: "https://emekaowo-portfolio.netlify.app",
     },
     {   
-        id: 7,
+        id: 8,
         img: "https://i.ibb.co/bzcX1CK/screencapture-127-0-0-1-5500-index-html-2024-08-03-16-20-04-1.png",
         link: "https://handy-codes.github.io/stop-watch/",
     },
     {
-        id: 8,
+        id: 9,
         img: "https://i.ibb.co/TbDg3c5/mekxus-Gofull.png",
         link: "https://mekxos-properties.onrender.com",  
     },
     {
-        id: 9,
+        id: 10,
         img: "https://i.ibb.co/McTVmhz/screencapture-handy-codes-github-io-owo-portfolio-2024-08-03-20-03-20.png",
         link: "https://handy-codes.github.io/owo-portfolio/",
     },
